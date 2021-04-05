44e412fddf557db2916295d7d4b871d8cc72b8f5
Ravshan Samandarov <ravshansbox@gmail.com>
2021-04-05 16:55:45 +0000

    Pipe incomning request to outgoing request

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ const { request: httpsRequest } = require('https');
 const { HTTP_PORT = '80' } = process.env;
 
 const server = createServer((req, res) => {
-  const querySignIndex = req.url.indexOf('?');
+  const { headers, method, url } = req;
+  const querySignIndex = url.indexOf('?');
   if (querySignIndex === -1) {
     return res.end();
   }
-  const urlPath = new URLSearchParams(req.url.substr(querySignIndex)).get('url');
-  const url = new URL(urlPath);
-  const request = url.protocol === 'http' ? httpRequest : httpsRequest;
-  delete req.headers.host;
-  request(urlPath, { method: req.method, headers: req.headers }, (message) => {
+  const urlPath = new URLSearchParams(url.substr(querySignIndex)).get('url');
+  const request = new URL(urlPath).protocol === 'http' ? httpRequest : httpsRequest;
+  delete headers.host;
+  const client = request(urlPath, { method, headers }, (message) => {
     for (const [key, value] of Object.entries(message.headers)) {
       res.setHeader(key, value);
     }
     message.pipe(res);
-  }).end();
+  });
+  req.pipe(client);
 });
 
 server.listen(HTTP_PORT, () => {
